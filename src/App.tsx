77208b7b964ecc92b47102a692ffc406c59b3091
Konstantin Prakhov <costa@Konstantins-MacBook-Pro.local>
2023-02-28 22:14:42 +0400

    fixing

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Leaderboard from "./Leaderboard";
 import Footer from "./Footer";
 import "./styles/Home.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import bg_udd from "./assets/udd_bg.png";
 
 export default function Home() {
   return (
@@ -13,6 +14,9 @@ export default function Home() {
       <main className="main">
         <Router>
           <Header />
+          <div className="image">
+            <img src={bg_udd} alt="bg_image"></img>
+          </div>
           <Routes>
             <Route path="/" element={<Main />}></Route>
             <Route path="/faq" element={<Faq />}></Route>
--- src/Main.tsx
+++ src/Main.tsx
@@ -4,34 +4,15 @@ import dialog1 from "./assets/dialog1.svg";
 import dialog2 from "./assets/dialog2.svg";
 
 export default function Main() {
-  const [imgSrc, setImgSrc] = useState(dialog1);
-  const [text, setText] = useState(
-    "HEY USELESS! IF U HAVE USELESS KEY ALREADY, JUST GO TO 'CLOSE THE DOOR' PAGE!"
-  );
-
-  const handleScroll = useCallback(() => {
-    const scrollPosition = window.scrollY;
-    if (scrollPosition > 200) {
-      setImgSrc(dialog2);
-      setText("This is the updated text");
-    } else {
-      setImgSrc(dialog1);
-      setText(
-        "HEY USELESS! IF U HAVE USELESS KEY ALREADY, JUST GO TO 'CLOSE THE DOOR' PAGE!"
-      );
-    }
-  }, [setImgSrc, setText]);
-
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, [handleScroll]);
-
   return (
     <div className="maindialog">
-      <div className="nav">
-        <img src={imgSrc} alt="dialog1" className="dialog1-img"></img>
-        <p className="dialog1-text">{text}</p>
+      <div className="nav2">
+        <img src={dialog1} alt="dialog1" className="dialog1-img"></img>
+        <p className="dialog1_text">HEY USELESS! IF U HAVE USELESS KEY ALREADY, JUST GO TO 'CLOSE THE DOOR' PAGE!</p>
+      </div>
+      <div className="nav3">
+        <img src={dialog2} alt="dialog1" className="dialog2-img"></img>
+        <p className="dialog1_text">HEY USELESS! IF U HAVE USELESS KEY ALREADY, JUST GO TO 'CLOSE THE DOOR' PAGE!</p>
       </div>
     </div>
   );
