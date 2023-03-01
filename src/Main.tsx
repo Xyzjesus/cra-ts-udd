89f246dad5c0984a5c67ed008895e0ef1fe98265
Konstantin Prakhov <costa@Konstantins-MacBook-Pro.local>
2023-03-01 16:21:00 +0400

    layout impr

--- src/Main.tsx
+++ src/Main.tsx
@@ -2,18 +2,81 @@ import React, { useState, useEffect, useCallback } from "react";
 import "./styles/Main.css";
 import dialog1 from "./assets/dialog1.svg";
 import dialog2 from "./assets/dialog2.svg";
+import dialog3 from "./assets/dialog3.svg";
+import scrollpic from "./assets/scroll_pic.png"
 
 export default function Main() {
+
+  function onEntry(entry: IntersectionObserverEntry[]) {
+    entry.forEach((change: IntersectionObserverEntry) => {
+      if (change.isIntersecting) {
+        change.target.classList.add('show');
+      }
+    });
+  }
+  
+  let options = { threshold: [0.5] };
+  let observer = new IntersectionObserver(onEntry, options);
+  let elements = document.querySelectorAll('.hided_nav');
+  for (let elm of Array.from(elements)) {
+    elm.classList.add('show');
+    observer.observe(elm);
+  }
+
   return (
+    <>
     <div className="maindialog">
-      <div className="nav2">
-        <img src={dialog1} alt="dialog1" className="dialog1-img"></img>
-        <p className="dialog1_text">HEY USELESS! IF U HAVE USELESS KEY ALREADY, JUST GO TO 'CLOSE THE DOOR' PAGE!</p>
+      
+      <div className="hided_nav">
+        <img src={dialog1} alt="dialog1" className="dialog1-img img"></img>
+        <p className="dialog1 text">HEY USELESS! IF U HAVE USELESS KEY ALREADY, JUST GO TO 'CLOSE THE DOOR' PAGE!</p>
       </div>
-      <div className="nav3">
-        <img src={dialog2} alt="dialog1" className="dialog2-img"></img>
-        <p className="dialog1_text">HEY USELESS! IF U HAVE USELESS KEY ALREADY, JUST GO TO 'CLOSE THE DOOR' PAGE!</p>
+      <div className="hided_nav">
+        <img src={dialog2} alt="dialog2" className="dialog2-img img"></img>
+        <p className="dialog2 text">IF U DIDN'T GET IT, SCROLL DOWN...</p>
       </div>
+
+      <div className="hided_nav">
+        <img src={dialog3} alt="dialog3" className="dialog3-img img"></img>
+        <p className="dialog3 text">I AM NEWBIE! WTF IS GOING ON?</p>
+      </div>
+      <div className="hided_nav">
+        <img src={dialog2} alt="dialog4" className="dialog4-img img"></img>
+        <p className="dialog4 text">HERE U CAN MINT OUR USELESS KEY...</p>
+      </div>
+
+      <div className="hided_nav">
+        <img src={dialog3} alt="dialog3" className="dialog3-img img"></img>
+        <p className="dialog3 text">WHAT'S THE USELESS KEY?</p>
+      </div>
+      <div className="hided_nav">
+        <img src={dialog2} alt="dialog4" className="dialog4-img img"></img>
+        <p className="dialog4 text">THIS IS OUR WELCOME NFT CREATED FOR A MINI GAME...</p>
+      </div>
+
+      <div className="hided_nav">
+        <img src={dialog3} alt="dialog3" className="dialog3-img img"></img>
+        <p className="dialog3 text">WHAT'S THE GAME?</p>
+      </div>
+      <div className="hided_nav">
+        <img src={dialog2} alt="dialog4" className="dialog4-img img"></img>
+        <p className="dialog4 text">IT'S INTERACRIVE, WHERE U NEED GET POINT BY CLOSING THE DOOR...</p>
+      </div>
+
+      <div className="hided_nav">
+        <img src={dialog3} alt="dialog3" className="dialog3-img img"></img>
+        <p className="dialog3 text">WHAT'S REASON?</p>
+      </div>
+      <div className="hided_nav">
+        <img src={dialog2} alt="dialog4" className="dialog4-img img"></img>
+        <p className="dialog4 text">MORE POINTS - MORE CHANCES TO GAIN WL FOR MAIN COLLECTION...</p>
+      </div>
+      
     </div>
+    
+    <div className="scrollpic">
+        <img src={scrollpic} alt="scroll_pic" width="53px" height="130px"></img>
+    </div>
+    </>
   );
 }
